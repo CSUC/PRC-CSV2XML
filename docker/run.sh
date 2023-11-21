553e8446ee8b76b0706925bfb29590c1cee4a742
Albert Martínez <albert.martinez@csuc.cat>
2023-11-21 12:00:07 +0100

    updated docker

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 
 cd /opt/spark/work-dir
 
-/opt/spark/bin/spark-submit --master "local[*]" --class org.csuc.cli.Cerif --packages info.picocli:picocli:4.6.3,com.crealytics:spark-excel_2.12:3.2.1_0.17.0,com.typesafe:config:1.4.2 --jars euroCRIS-cerif-definitions.jar prc-cerif.jar "$@"
+/opt/spark/bin/spark-submit --master "local[*]" --class org.csuc.cli.Cerif --packages info.picocli:picocli:4.7.5,com.crealytics:spark-excel_2.12:3.5.0_0.20.2,com.typesafe:config:1.4.3 --jars euroCRIS-cerif-definitions.jar prc-cerif.jar "$@"
